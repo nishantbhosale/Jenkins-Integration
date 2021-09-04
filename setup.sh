33b92aee3b9bf466029d8b759be17f95e158a032
Nishant Bhosale <bhosale.nishant.n@gmail.com>
2021-09-05 01:54:32 +0530

    ssh command updation

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Hello from github"
 sudo scp -i /home/ec2-user/arth-aws.pem /var/lib/jenkins/workspace/Jenkins-GitHub\ Integration/* ec2-user@52.207.64.42:/home/ec2-user/code
-sudo ssh -i /home/ec2-user/arth-aws.pem ec2-user@52.207.64.42
-sudo mv /home/ec2-user/code/* /var/www/html
-sudo systemctl restart httpd
+sudo ssh -i /home/ec2-user/arth-aws.pem ec2-user@52.207.64.42 sudo mv /home/ec2-user/code/* /var/www/html
+sudo ssh -i /home/ec2-user/arth-aws.pem ec2-user@52.207.64.42 sudo systemctl restart httpd
