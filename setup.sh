9a2cfec9050426e14b5b479e842d42eb73e8b9ea
Nishant Bhosale <bhosale.nishant.n@gmail.com>
2021-09-05 01:47:59 +0530

    ssh to the system, copy file, and restart the httpd server

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "Hello from github"
-sudo scp -i /home/ec2-user/arth-aws.pem /var/lib/jenkins/workspace/Jenkins-GitHub\ Integration/* ec2-user@52.207.64.42:/home/ec2-user/
-
+sudo scp -i /home/ec2-user/arth-aws.pem /var/lib/jenkins/workspace/Jenkins-GitHub\ Integration/* ec2-user@52.207.64.42:/home/ec2-user/code
+sudo ssh -i /home/ec2-user/arth-aws.pem ec2-user@52.207.64.42
+sudo mv /home/ec2-user/code/* /var/www/html
+sudo systemctl restart httpd
